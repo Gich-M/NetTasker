2b1eb70ef7cc3b1a527a39a7b237f3590db6d90b
Gich-M <gichmuriuki21@gmail.com>
2024-08-30 10:11:39 +0300

    C-Workers_v1

---
+++ src/worker/worker.h
@@ -0,0 +1,32 @@
+#ifndef WORKER_H
+#define WORKER_H
+
+#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+
+#define MAX_BUFFER_SIZE 1024
+#define DEFAULT_NUM_WORKERS 1
+#define CONFIG_FILE_PATH "config/processor_config.ini"
+
+typedef struct {
+    char *host;
+    int port;
+} worker_args;
+
+
+void init_python(void);
+void finalize_python(void);
+PyObject* process_task(PyObject* task);
+PyObject *pModule, *pFunc;
+void run_worker(const char* host, int port);
+void* worker_thread(void* arg);
+int get_num_workers(void);
+
+#endif WORKER_H /* WORKER_H */
