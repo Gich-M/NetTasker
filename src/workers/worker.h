d78720e6d118d88f1b8458d36420615afebac9c4
Gich-M <gichmuriuki21@gmail.com>
2024-09-21 13:14:58 +0300

    Header file with function prototypes, struct and constants

--- src/workers/worker.h
+++ src/workers/worker.h
@@ -2,31 +2,53 @@
 #define WORKER_H
 
 #include <Python.h>
+#include <curl/curl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+#include <json-c/json.h>
 
+#define MAX_URL_LENGTH 256
+#define RETRY_DELAY 5
+#define TASK_DELAY 1
+#define DEFAULT_TASK_TYPE "ip_data"
+/* 
+ * Structure to hold memory for CURL write callback: 
+ * @memory: A pointer to dynamically allocated memory, 
+ * @size: The current size of allocated memory 
+ * */
+typedef struct MemoryStruct {
+    char *memory;
+    size_t size;
+} MemoryStruct;
 
-#define MAX_BUFFER_SIZE 1024
-#define DEFAULT_NUM_WORKERS 1
-#define CONFIG_FILE_PATH "config/processor_config.ini"
+/* Function prototypes */
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp);
+static void handle_curl_error(CURL *curl, CURLcode res);
+static PyObject *setup_python_config(void);
+static PyObject *setup_task_processor(PyObject *pConfigInstance);
+static PyObject *setup_asyncio(void);
+static int parse_task_json(const char *task, json_object **task_content,
+                           json_object **ip_address, json_object **task_id,
+                           json_object **client_ip, json_object **task_type);
+static PyObject *create_python_task_dict(json_object *ip_address,
+                                         json_object *task_id,
+                                         json_object *client_ip,
+                                         json_object *task_type);
+static int process_task(PyObject *pFunc, json_object *task);
+static int request_and_process_task(CURL *curl, const char *host, int port,
+                                    const char *worker_id, MemoryStruct *chunk,
+                                    PyObject *pFunc);
+static json_object *parse_json(const char *response);
+static int process_json_task(json_object *task_obj, PyObject *pFunc);
+static int reallocate_chunk_memory(MemoryStruct *chunk);
 
-typedef struct {
-    char *host;
-    int port;
-} worker_args;
-
-
-void init_python(void);
-void finalize_python(void);
-PyObject* process_task(PyObject* task);
-PyObject *pModule, *pFunc;
-void run_worker(const char* host, int port);
-void* worker_thread(void* arg);
-int get_num_workers(void);
+PyObject *init_python(void);
+int init(CURL **curl, PyObject **pFunc, MemoryStruct *chunk);
+int request_task(CURL *curl, const char *host, int port, const char *worker_id, MemoryStruct *chunk, const char *task_type);
+char *process_tasks(PyObject *pFuncTuple, const char *task);
+int submit_result(CURL *curl, const char *host, int port, const char *worker_id, const char *result_str, const char *task_type);
+int main(int argc, char *argv[]);
 
 #endif
